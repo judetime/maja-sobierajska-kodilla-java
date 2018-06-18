1fc081adff484c42ff8ec2ff8ecc9fce34c5f189
Maja Sobierajska <maja@MacBook-Air-Maja.local>
2018-06-18 20:32:24 +0200

    opis

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(1, 2);
+
+        if (sum == 1 + 2) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Not OK");
+        }
+
+        int diff = calculator.subtract(3, 1);
+
+        if (diff == 3 - 1) {
+            System.out.println("Fine");
+        } else {
+            System.out.println("Not Fine");
+        }
+    }
 }
