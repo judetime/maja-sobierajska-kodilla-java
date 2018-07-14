c10c08843fcf510f5c1db1cfab69dd6eb09de500
Maja Sobierajska <maja@MacBook-Air-Maja.local>
2018-07-14 21:14:29 +0200

    ArrayList OddNumbersExterminator

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,62 @@
+package com.kodilla.testing.collection;
+import javafx.beans.binding.When;
+import org.junit.*;
+import java.util.*;
+import org.junit.rules.*;
+
+import static org.junit.Assert.*;
+
+public class CollectionTestSuite {
+
+    @Before
+    public void before(){
+        System.out.println("Test Case: begin");
+    }
+    @After
+    public void after(){
+        System.out.println("Test Case: end");
+    }
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Test Suite: begin");
+    }
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Test Suite: end");
+    }
+    @Rule
+    public final TestName name = new TestName();
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList ()
+    {
+        //wyswietl
+        System.out.println(name.getMethodName());
+        //Given
+        ArrayList<Integer> emptyList = new ArrayList<Integer>();
+        //When
+        ArrayList<Integer> actuallList = OddNumbersExterminator.exterminate(emptyList);
+        //Then
+        Assert.assertEquals(emptyList, actuallList);
+
+    }
+    @Rule
+    public final TestName name2 = new TestName();
+
+    @Test
+    public void testOddNumbersExterminatorNormalList()
+    {
+        //wyswietl
+        System.out.println(name2.getMethodName());
+        //Given
+        ArrayList<Integer> oddAndEvenList = new ArrayList<Integer>();
+        //When
+        ArrayList<Integer> actuallList = OddNumbersExterminator.exterminate(oddAndEvenList);
+        //Then
+        Assert.assertEquals(oddAndEvenList, actuallList);
+    }
+
+
+
+
+}
